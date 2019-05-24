f4baace7bffe993bcf789e35176db611eedbb93f
Trai Lynne <trailynne@gmail.com>
2019-05-24 10:42:37 -0600

    Pseudocode ring_buffer `append` method

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -6,6 +6,12 @@ class RingBuffer:
 
   def append(self, item):
     pass
+    # Set the current index of storage as the item
+
+    # if the current index is less than the capacity (minus 1 because of how indexes are numbered), 
+    #   move to the next index, 
+    # else 
+    #   start over
 
   def get(self):
     # For each element in storage, add element to list if item != None. Return list
