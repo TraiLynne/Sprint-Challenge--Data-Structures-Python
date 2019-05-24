edfb1ef698ad2770207a501751634550567b7fb8
Trai Lynne <trailynne@gmail.com>
2019-05-24 10:48:24 -0600

    Solve ring_buffer `append` method Set current index of stoarge as provided item Increase index if less than capacity, else start over at 0 All tests pass !!!!!!

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -5,13 +5,14 @@ class RingBuffer:
     self.storage = [None]*capacity
 
   def append(self, item):
-    pass
     # Set the current index of storage as the item
+    self.storage[self.current] = item
 
-    # if the current index is less than the capacity (minus 1 because of how indexes are numbered), 
-    #   move to the next index, 
-    # else 
-    #   start over
+    # if the current index is less than the capacity (minus 1 because of how indexes are numbered), move to the next index. Else, start over
+    if self.current < self.capacity-1:
+      self.current += 1
+    else:
+      self.current = 0
 
   def get(self):
     # For each element in storage, add element to list if item != None. Return list
